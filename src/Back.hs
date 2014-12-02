cae306cf31deebbc4fd9597dbb9052206ae5dd9d
RoboNickBot <nicklewchenko92@gmail.com>
2014-12-02 13:30:13 -0600

    preliminary

--- src/Back.hs
+++ src/Back.hs
@@ -1,14 +1,16 @@
-
 import Network.CGI( CGI, CGIResult, setHeader, readInput, output, getInputs)
 import Network.FastCGI( runFastCGI )
 import Control.Applicative
 
 import qualified Crubadan.Search as S
+import qualified Crubadan.Types as T
 
-cgiMain :: CGI CGIResult
-cgiMain = do setHeader "Content-type" "text/plain"
-             query <- readInput "query"
-             output $ show $ S.genResults <$> query <*> (pure S.database)
+cgiMain :: T.Database -> CGI CGIResult
+cgiMain d = do setHeader "Content-type" "text/plain"
+               query <- readInput "query"
+               output $ show $ fmap (take 20) $ S.genResults <$> query <*> (pure d)
 
 main :: IO ()
-main = runFastCGI $ cgiMain
+main = do d <- S.database "./db"
+          runFastCGI $ cgiMain d
+          --(print . show . S.genResults "a") d
