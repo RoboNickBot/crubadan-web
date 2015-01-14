27b1dd0a3cdcd10d62127b66f2a96dbadcfebd99
RoboNickBot <nicklewchenko92@gmail.com>
2015-01-13 21:24:04 -0600

    temp

--- back-end/src/Crubadan/Search.hs
+++ back-end/src/Crubadan/Search.hs
@@ -25,7 +25,7 @@ genResults q = let trans (key,val) = ( key, (mayRegex val) )
                in crunch q . filter (matches (fmap trans q)) 
 
 fixQ s = let n = fmap toLower s
-         in "^" ++ n
+         in " " ++ n
 
 mayRegex "" = Nothing
 mayRegex s = Just (mkRegex (fixQ s))
@@ -36,7 +36,7 @@ matches r ws = (and . fmap isJust . fmap (matchAttr ws)) r
 matchAttr :: C.WS -> (String, Maybe Regex) -> Maybe [String]
 matchAttr ws (s, Nothing) = Just ["pass"]
 matchAttr ws (s,(Just r)) = 
-  M.lookup s (C.wsData ws) >>= matchRegex r . fmap toLower . C.wsaMatch
+  M.lookup s (C.wsData ws) >>= matchRegex r . fmap ((" " ++) . toLower) . C.wsaMatch
 
 crunch :: CS.Query -> [C.WS] -> [CS.Result]
 crunch q = foldr (\w -> (:) (C.wsUID w, pullVals q w)) []
